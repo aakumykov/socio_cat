e411de6fd5f6c486f5f74847eda6c7219e6ab0e3
Андрей Кумыков <aakumykov@yandex.ru>
2015-11-12 09:07:20 +0700

    Создан проект.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SocioCat::Application.config.secret_key_base = 'f03cd094754d3d5622e5f67b713c0a68202baf70031c05cac41798b05be405fb5849ed1804f0c66f3d9646f52c4a76cd43d283ae88528ed232243f4684b3106e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SocioCat::Application.config.session_store :cookie_store, key: '_socio_cat_session'
