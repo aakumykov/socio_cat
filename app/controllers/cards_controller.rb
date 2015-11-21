57f363d20f83db6282b04a4d321e1da052504445
Андрей Кумыков <aakumykov@yandex.ru>
2015-11-22 02:36:00 +0700

    Тесты и некрасивое отображение списка карточек.

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,7 @@
 class CardsController < ApplicationController
+	
 	def index
+		@all_cards = Card.all
 	end
 
 	
--- spec/requests/card_pages_spec.rb
+++ spec/requests/card_pages_spec.rb
@@ -8,6 +8,16 @@ describe 'Стриницы карточек' do
 
 		it { should have_title('Список карточек') }
 		it { should have_selector('h1','Список карточек') }
+		
+		describe 'одна карточка,' do
+			let(:card_title) { '1st card title' }
+			let(:card_content) { 'This is content of 1st card.' }
+			let(:card) { FactoryGirl.create(:card, title: card_title, content: card_content) }
+			
+			it { should have_selector('fieldset.card') }
+			it { should have_selector('legend.card_title',text: card_title) }
+			it { should have_selector('div.card_content', text: card_content) }
+		end
 	end
 
 end
