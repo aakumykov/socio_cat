60db9b1e4684ddca3d31270c4275af7f09d836f9
Андрей Кумыков <aakumykov@yandex.ru>
2015-11-21 12:19:50 +0700

    Свойства title, content, их проверки.

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,50 @@
 require 'spec_helper'
 
-describe Card do
-  pending "add some examples to (or delete) #{__FILE__}"
+# (создание тестового объекта)
+# прямые и связанные свойства модели
+# валидность и другие свойства (be_)
+# проверка того, что валидации работают
+# отдельный метод
+# поведение свойства
+# поведение свойства
+# отдельный случай поведения
+# работа связей с другими моделями
+
+describe 'Карточка,' do
+
+	# before(:each) do
+	# 	@card = Card.new(
+	# 		title: 'Проверочная карточка',
+	# 		content: 'Какое-нибудь содержимое',
+	# 	)
+	# end
+	# subject { @card }
+
+	# тестовая модель
+	let(:card) {
+		Card.new(
+			title: 'Проверочная карточка',
+			content: 'Какое-нибудь содержимое',
+		)
+	}
+	subject { card }
+
+
+	# свойства
+	it { should respond_to(:title) }
+	it { should respond_to(:content) }
+
+
+	# валидации
+	it { should be_valid }
+
+	describe 'когда отсутствует заголовок,' do
+		before { card.title = ' ' }
+		it { should_not be_valid }
+	end
+
+	describe 'когда отсутствует содержимое,' do
+		before { card.content = ' ' }
+		it { should_not be_valid }
+	end
 end
