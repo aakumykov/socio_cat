f0f1bd9e495f5072b9b7d57f24c798ba21fba109
Андрей Кумыков <aakumykov@yandex.ru>
2015-11-22 23:38:59 +0700

    Тест для ApplicationHelper.

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,8 +1,17 @@
 require 'spec_helper'
 
 describe ApplicationHelper do
+
 	describe 'full_title' do
-		it 'должен включать базовый заголовок'
-		it 'должен включать заголовок страницы'
+		let(:base_title) {'SocioCat: '}
+		let(:title) {'раздел одну'}
+
+		it 'должен включать базовый заголовок' do
+			expect( full_title(title) ).to match("^#{base_title}")
+		end
+
+		it 'должен включать заголовок страницы' do
+			expect( full_title(title) ).to match(": #{title}$")
+		end
 	end
 end
