fa71fe415d61d9861e4c6d905abc3c2756c1636a
Андрей Кумыков <aakumykov@yandex.ru>
2015-11-22 23:21:25 +0700

    Тесты наличия полного заголовка.

--- spec/requests/welcome_spec.rb
+++ spec/requests/welcome_spec.rb
@@ -4,7 +4,6 @@ describe 'Статические welcome-страницы,' do
 	subject { page }
 	
 	shared_examples_for 'all_welcome_pages' do
-		it 'Соответствующий сложный <title>'
 		it { should have_link('Главная страница', href: home_path) }
 		it { should have_link('О проекте', href: about_path) }
 		it { should have_link('Помощь', href: help_path) }
@@ -13,19 +12,24 @@ describe 'Статические welcome-страницы,' do
 
 	describe 'Главная страница,' do
 		before { visit root_path }
+		it { should have_title(full_title('Главная страница')) }
 		it { should have_selector('h1', text:'Добро пожаловать') }
 		it_should_behave_like 'all_welcome_pages'
 	end
 
 	describe 'О проекте,' do
+		let(:title) { 'О проекте' }
 		before { visit about_path }
-		it { should have_selector('h1', text:'О проекте') }
+		it { should have_title(full_title(title)) }
+		it { should have_selector('h1', text:title) }
 		it_should_behave_like 'all_welcome_pages'
 	end
 
 	describe 'Помощь,' do
+		let(:title) { 'Помощь' }
 		before { visit help_path }
-		it { should have_selector('h1', text:'Помощь') }
+		it { should have_title(full_title(title)) }
+		it { should have_selector('h1', text:title) }
 		it_should_behave_like 'all_welcome_pages'
 	end
 end
