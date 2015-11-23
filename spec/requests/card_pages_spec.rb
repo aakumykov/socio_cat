b5782c8f7d9fa7344346628855f2dfcbc9e380f7
Андрей Кумыков <aakumykov@yandex.ru>
2015-11-24 00:38:55 +0700

    Интеграционный тест создания карточки.

--- spec/requests/card_pages_spec.rb
+++ spec/requests/card_pages_spec.rb
@@ -25,18 +25,51 @@ describe 'Стриницы карточек' do
 	end
 
 	describe 'создание карточки,' do
+
 		before(:each) { visit new_card_path }
 
-		it { should have_title(full_title('Новая карточка')) }
-		it { should have_selector('h1', text:'Новая карточка') }
+		
+		describe 'статические элементы,' do
+			it { should have_title(full_title('Новая карточка')) }
+			it { should have_selector('h1', text:'Новая карточка') }
 
-		it { should have_selector('label', text:'Название') }
-		it { should have_selector(:xpath,'//input[@name="card[title]"]') }
+			it { should have_selector('label', text:'Название') }
+			it { should have_selector(:xpath,'//input[@name="card[title]"]') }
 
-		it { should have_selector('label', text:'Содержимое') }
-		it { should have_selector(:xpath,'//textarea[@name="card[content]"]') }
+			it { should have_selector('label', text:'Содержимое') }
+			it { should have_selector(:xpath,'//textarea[@name="card[content]"]') }
 
-		it { should have_selector(:xpath,'//input[@value="Создать" and @name="commit"]') }
+			it { should have_selector(:xpath,'//input[@value="Создать" and @name="commit"]') }
+		end
+
+
+		describe 'действия,' do
+			
+			describe 'с неверными данными,' do
+				it 'карточка не должна создаваться'
+				it 'должно выводиться сообщение об ошибке'
+				it 'должна появляться форма редактирования'
+			end
+
+			describe 'с верными данными,' do
+				before(:each) do
+					fill_in 'Название', with: 'Проверочный заголовок'
+					fill_in 'Содержимое', with: 'Проверочное наполнение (соционической функции).'
+				end
+
+				it 'должна создаваться карточка,' do
+					expect { click_button 'Создать'}.to change(Card, :count).by(1)
+				end
+
+				# expect 'должна создаваться карточка,' do
+				# 	click_button 'Создать'
+				# end.to change(Card, :count).by(1)
+
+				it 'должно выводиться сообщение об успехе'
+				it 'должна отображаться вновь созданная карточка'
+			end
+
+		end
 	end
 
 end
