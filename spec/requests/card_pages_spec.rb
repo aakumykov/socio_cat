a50a0dcd982b5dbdc97cef27ecf2172ad3ea2348
Андрей Кумыков <aakumykov@yandex.ru>
2015-11-25 01:45:28 +0700

    Улучшение card_pages_spec, тест для показа одной карточки.

--- spec/requests/card_pages_spec.rb
+++ spec/requests/card_pages_spec.rb
@@ -3,6 +3,30 @@ require 'spec_helper'
 describe 'Стриницы карточек,' do
 	subject { page }
 
+	let(:card_title) { '1st card title' }
+	let(:card_content) { 'This is content of 1st card.' }
+
+	shared_examples_for 'карточка' do
+		it { should have_selector('fieldset.card') }
+		it { should have_selector('legend.card_title',text: card_title) }
+		it { should have_selector('div.card_content', text: card_content) }
+	end
+
+	shared_examples_for 'форма редактирования карточки' do
+		it { should have_title(full_title('Новая карточка')) }
+		it { should have_selector('h1', text:'Новая карточка') }
+
+		it { should have_selector('form#new_card') }
+
+		it { should have_selector('label', text:'Название') }
+		it { should have_selector(:xpath,'//input[@name="card[title]"]') }
+
+		it { should have_selector('label', text:'Содержимое') }
+		it { should have_selector(:xpath,'//textarea[@name="card[content]"]') }
+
+		it { should have_selector(:xpath,'//input[@value="Создать" and @name="commit"]') }
+	end
+
 	describe 'список,' do
 		before(:each) { visit cards_path }
 
@@ -11,38 +35,18 @@ describe 'Стриницы карточек,' do
 		it { should have_link('Новая', href: new_card_path) }
 		
 		describe 'одна карточка,' do
-			let(:card_title) { '1st card title' }
-			let(:card_content) { 'This is content of 1st card.' }
-			
 			before(:each) {
 				FactoryGirl.create(:card, title: card_title, content: card_content)
 				visit cards_path
 			}
 			
-			it { should have_selector('fieldset.card') }
-			it { should have_selector('legend.card_title',text: card_title) }
-			it { should have_selector('div.card_content', text: card_content) }
+			it_should_behave_like 'карточка'
 		end
 	end
 
 	describe 'создание,' do
 
 		before(:each) { visit new_card_path }
-
-		shared_examples_for 'форма редактирования карточки' do
-			it { should have_title(full_title('Новая карточка')) }
-			it { should have_selector('h1', text:'Новая карточка') }
-
-			it { should have_selector('form#new_card') }
-
-			it { should have_selector('label', text:'Название') }
-			it { should have_selector(:xpath,'//input[@name="card[title]"]') }
-
-			it { should have_selector('label', text:'Содержимое') }
-			it { should have_selector(:xpath,'//textarea[@name="card[content]"]') }
-
-			it { should have_selector(:xpath,'//input[@value="Создать" and @name="commit"]') }
-		end
 
 		describe 'статика,' do
 			it_should_behave_like 'форма редактирования карточки'
@@ -80,7 +84,11 @@ describe 'Стриницы карточек,' do
 					it { should have_selector('div.alert.alert-success', text:'Карточка создана') }
 				end
 
-				it 'должна отображаться вновь созданная карточка'
+				describe 'должна отображаться вновь созданная карточка,' do
+					before { click_button 'Создать' }
+					it { should have_selector('h1',text:'Просмотр карточки') }
+					it_should_behave_like 'карточка'
+				end
 			end
 
 		end
