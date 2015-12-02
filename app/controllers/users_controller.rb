eb39b05a72ccfdf341172472f48d0b43c179a433
Андрей Кумыков <aakumykov@yandex.ru>
2015-12-03 00:41:37 +0700

    Создание пользователя в штатном режиме.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,34 @@
 class UsersController < ApplicationController
-  def new
-  	@user = User.new
-  end
+
+	public
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.create(user_params)
+		if nil==@user
+			flash.now[:error] = 'ОШИБКА: пользователь не создан'
+			render 'new'
+		else
+			flash[:success] = "Создан пользователь «#{@user.name}»"
+			redirect_to user_path(@user)
+		end
+	end
+
+	def show
+		@user = User.find_by(id: params[:id])
+	end
+
+
+	private
+
+	def user_params
+		params.require(:user).permit(
+			:name,
+			:email,
+		)
+	end
+
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
 describe 'Страницы пользователя,' do
-	before(:all) {
-		@user = FactoryGirl.create(:user)
-	}
+
+	let(:create_button) { 'Создать' }
+
 	subject { page }
 
-	describe 'создание пользователя,' do
+	describe 'создание,' do
 		before { visit new_user_path }
 
-		describe 'форма,' do
+		describe 'отображение формы,' do
 			it { should have_title(full_title('Регистрация пользователя')) }
 			it { should have_selector('h1',text:'Регистрация пользователя') }
 
@@ -22,6 +22,40 @@ describe 'Страницы пользователя,' do
 			it { should have_selector(:xpath, "//input[@value='Создать']") }
 		end
 
+		describe 'работа формы,' do
+			
+			describe 'с верными данными,' do
+				before {
+					fill_in 'Имя', with: 'Человек'
+					fill_in 'Электронная почта', with: 'homo@sapiens.me'
+				}
+				
+				it 'появление пользователя,' do
+					expect{ click_button(create_button) }.to change(User, :count).by(1)
+				end
+				
+				describe 'сообщение об успехе,' do
+					before { 
+						click_button(create_button) 
+						@user = User.last
+					}
+					it { should have_selector('.alert.alert-success', text:"Создан пользователь «#{@user.name}»") }
+				end
+				
+				describe 'перенаправление на страницу пользователя,' do
+					before { click_button(create_button) }
+					it { should have_title(full_title('Страница пользователя')) }
+					it { should have_selector('h1',text:'Страница пользователя') }
+				end
+			end
+			
+			# describe 'с неверными данными,' do
+			# 	it 'отклонение нового пользователя,' do
+			# 		expect{ click_button('Создать') }.not_to change(User,:count)
+			# 	end
+			# 	it { should have_selector('.alert.alert-error', text:'ОШИБКА: пользователь не создан') }
+			# end
+		end
 	end
 
 end
