df79ade3dda2c3a35d1bdf4df7819b656fcc6cd3
Андрей Кумыков <aakumykov@yandex.ru>
2015-12-02 23:47:16 +0700

    Страница создания пользователя.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,16 +8,20 @@ describe 'Страницы пользователя,' do
 
 	describe 'создание пользователя,' do
 		before { visit new_user_path }
-		it { should have_title(full_title('Регистрация пользователя')) }
-		it { should have_selector('h1',text:'Регистрация пользователя') }
 
-		it { should have_selector('lebel',text:'Имя') }
-		it { should have_selector(:xpath, "//input[@name='user[name]']") }
+		describe 'форма,' do
+			it { should have_title(full_title('Регистрация пользователя')) }
+			it { should have_selector('h1',text:'Регистрация пользователя') }
 
-		it { should have_selector('label', text:'Электронная почта') }
-		it { should have_selector(:xpath, "//input[@name='user[email]']") }
+			it { should have_selector('label',text:'Имя') }
+			it { should have_selector(:xpath, "//input[@name='user[name]']") }
 
-		it { should have_selector(:xpath, "//input[@value='Создать']") }
+			it { should have_selector('label', text:'Электронная почта') }
+			it { should have_selector(:xpath, "//input[@name='user[email]']") }
+
+			it { should have_selector(:xpath, "//input[@value='Создать']") }
+		end
+
 	end
 
 end
