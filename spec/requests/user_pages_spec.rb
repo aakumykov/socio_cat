ce1c96af424c18fdcfa7afb9023a710f1c2bfcb2
Андрей Кумыков <aakumykov@yandex.ru>
2015-12-02 23:00:58 +0700

    Что-то нет полей в форме пользователя.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,15 +2,22 @@ require 'spec_helper'
 
 describe 'Страницы пользователя,' do
 	before(:all) {
-		let(:user) { FactoryGirl.create(:user) }
+		@user = FactoryGirl.create(:user)
 	}
 	subject { page }
 
 	describe 'создание пользователя,' do
 		before { visit new_user_path }
-		#subject { page }
 		it { should have_title(full_title('Регистрация пользователя')) }
-		it { should have_selector('h1','Регистрация пользователя') }
+		it { should have_selector('h1',text:'Регистрация пользователя') }
+
+		it { should have_selector('lebel',text:'Имя') }
+		it { should have_selector(:xpath, "//input[@name='user[name]']") }
+
+		it { should have_selector('label', text:'Электронная почта') }
+		it { should have_selector(:xpath, "//input[@name='user[email]']") }
+
+		it { should have_selector(:xpath, "//input[@value='Создать']") }
 	end
 
 end
