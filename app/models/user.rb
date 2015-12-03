cf5b908004ed11b66ca5668504c6c71fa45ea087
Андрей Кумыков <aakumykov@yandex.ru>
2015-12-03 10:06:45 +0700

    Заготовки валидаций в User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+	validates :name, { presence: true,
+		#uniqueness: { case_sensitive: false },
+		#langth: { maximum:20 },
+	}
+
+	validates :email, { presence: true,
+		#uniqueness: { case_sensitive: true },
+		#format: { with: VALID_EMAIL_REGEX },
+	}
+
 end
