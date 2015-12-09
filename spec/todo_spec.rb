5e1dc0d21bfa6396157be4da7bed7342a9d17e9e
Андрей Кумыков <aakumykov@yandex.ru>
2015-12-09 18:17:28 +0700

    *

--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -5,4 +5,5 @@ describe 'TODO,' do
 	pending '"Страница пользователя «123»" вместо "Страница пользователя".'
 	pending 'Users#update опасный метод update_attributes.'
 	pending 'Ошибка "Почта уже используется" при редактировании пользователя.'
+	pending 'Как насчёт валидации для remember_token?'
 end
