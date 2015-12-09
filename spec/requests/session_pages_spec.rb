336b6792472bc2d82f54d250d7ee80da73e1bb28
Андрей Кумыков <aakumykov@yandex.ru>
2015-12-09 15:18:23 +0700

    Улучшение теста страниц сессий, заготовка теста страницы входа.

--- spec/requests/session_pages_spec.rb
+++ spec/requests/session_pages_spec.rb
@@ -1,25 +1,48 @@
 require 'spec_helper'
 
 describe 'Сессии,' do
+
+	let(:login_title) { 'Вход на сайт' }
+	let(:login_button) { 'Войти' }
+
+	shared_examples_for 'страница входа' do
+		it { should have_title(full_title(login_title)) }
+		it { should have_selector('h1',text:login_title) }
+		
+		it { should have_selector('label',text:'Электронная почта') }
+		it { should have_selector(:xpath,"//input[@id='session_email']") }
+
+		it { should have_selector('label',text:'Пароль') }
+		it { should have_selector(:xpath,"//input[@id='session_password']") }
+		
+		it { should have_selector(:xpath,"//input[@type='submit']") }
+		it { should have_selector(:xpath,"//input[@value='#{login_button}']") }
+	end
+
 	describe 'страница входа,' do
 		before { visit login_path }
 		subject { page }
 
-		let(:login_title) { 'Вход на сайт' }
-		let(:login_button) { 'Войти' }
+		describe 'строение,' do
+			it_should_behave_like 'страница входа'
+		end
 
-		describe 'вид,' do
-			it { should have_title(full_title(login_title)) }
-			it { should have_selector('h1',text:login_title) }
-			
-			it { should have_selector('label',text:'Электронная почта') }
-			it { should have_selector(:xpath,"//input[@id='session_email']") }
-
-			it { should have_selector('label',text:'Пароль') }
-			it { should have_selector(:xpath,"//input[@id='session_password']") }
-			
-			it { should have_selector(:xpath,"//input[@type='submit']") }
-			it { should have_selector(:xpath,"//input[@value='Войти']") }
+		describe 'функция,' do
+			let(:user_name) {'Пользователь Вася'}
+			let(:user_email) {'vasya@user.com'}
+			let(:user_password) {'Qwerty123!@#'}
+			before {
+				@user = FactoryGirl.create(:user, 
+					name: user_name,
+					email: user_email,
+					password: user_password,
+					password_confirmation: user_password,
+				)
+				visit login_path
+				fill_in 'Электронная почта', with: user_email
+				fill_in 'Пароль', with: user_password
+				click_button(login_button)
+			}
 		end
 	end
 
