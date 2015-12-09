f89d35b8f4ec2a057c08efa4ef1676a577b534c7
Андрей Кумыков <aakumykov@yandex.ru>
2015-12-09 16:48:56 +0700

    Дополнил TODO.

--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -3,4 +3,6 @@ require 'spec_helper'
 describe 'TODO,' do
 	pending 'Прямой доступ через HTTP-запросы.'
 	pending '"Страница пользователя «123»" вместо "Страница пользователя".'
+	pending 'Users#update опасный метод update_attributes.'
+	pending 'Ошибка "Почта уже используется" при редактировании пользователя.'
 end
