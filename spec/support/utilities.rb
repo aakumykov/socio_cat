d1040898601b96de99ae04ad1c5b89ec56ab6d54
Андрей Кумыков <aakumykov@yandex.ru>
2015-12-11 22:53:38 +0700

    Тень для элементов H1.

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,16 @@
 include ApplicationHelper
+
+def sign_in(:no_capybara=false)
+	let(:user) { FactoryGirl.create(:user) }
+	
+	if true==:no_capybara
+		remember_token = User.new_remember_token
+		user.update_attribute(:remember_token, User.encrypt(remember_token))
+		cookie.permanent[:remember_token] = remember_token
+	else	
+		visit login_path 
+		fill_in 'Электронная почта', with: user.name
+		fill_in 'Пароль', with: user.password
+		click_button 'Войти'
+	end
+end
