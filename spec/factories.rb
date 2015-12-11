0f3e6bf3ee2afb658ed4a62543ba76ba811d1851
Андрей Кумыков <aakumykov@yandex.ru>
2015-12-11 13:06:38 +0700

    FactoryGirl с использованием Faker.

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,21 +4,21 @@ FactoryGirl.define do
 		content 'Некое содержимое. Правда-правда, это некоторое содержимое. Очень странное, но очень настоящее содержимое.'
 	end
 
-	# factory :user do		
-	# 	name Faker::Name
-	# 	email Faker::Internet.email
+	# factory :user do
+	# 	sequence(:name)  { |n| "Пользователь #{n}" }
+	# 	sequence(:email) { |n| "user_#{n}@example.com"}
 	# 	password 'Password123$%^'
 	# 	password_confirmation 'Password123$%^'
+
+	# 	# factory :admin do
+	# 	# 	admin true
+	# 	# end
 	# end
 
 	factory :user do
-		sequence(:name)  { |n| "Пользователь #{n}" }
-		sequence(:email) { |n| "user_#{n}@example.com"}
-		password 'Password123$%^'
+		name                  { Faker::Name.first_name }
+		email                 { Faker::Internet.email }
+		password              'Password123$%^'
 		password_confirmation 'Password123$%^'
-
-		# factory :admin do
-		# 	admin true
-		# end
 	end
 end
