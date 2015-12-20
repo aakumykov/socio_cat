8812b510268cd0c7f19c6e7104749c0e4019891a
Андрей Кумыков <aakumykov@yandex.ru>
2015-12-20 14:21:15 +0700

    let(:the_user) плохонько, но работает.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -24,64 +24,15 @@ describe 'Страницы пользователя,' do
 		it { should_not have_selector("div.alert.alert-#{mode}") }
 	end
 
-	shared_examples_for 'страница' do |title,heading|
-		heading ||= title
-		it { should have_title( full_title(title) ) }
-		it { should have_selector('h1',text:heading) }
-	end
-
-	shared_examples_for 'главная страница' do
-		it_should_behave_like 'страница' do
-			let(:title) { 'Соционический каталог' }
-			let(:heading) { 'Добро пожаловать' }
-		end
+	shared_examples_for 'страница c названием' do
+		#it { should have_title( full_title(the_title) ) }
+		it { should have_title( full_title("Страница пользователя «#{the_user.name}»") ) }
 	end
 
 	shared_examples_for 'страница входа' do
-		it_should_behave_like 'страница' do
-			let(:title) { 'Вход на сайт' }
-		end
+		#it_should_behave_like 'страница с названием', title:'Вход на сайт'
 		it { should have_selector(:xpath,"//input[@type='submit']")}
 		it { should have_selector(:xpath,"//input[@value='Войти']")}
-	end
-
-	shared_examples_for 'требование входа' do
-		it_should_behave_like 'страница входа'
-		it_should_behave_like 'появление flash-сообщения', 'notice', 'Сначала войдите на сайт'
-	end
-
-	shared_examples_for 'страница пользователя' do |name|
-		it_should_behave_like 'страница' do
-			let(:title) { "Страница пользователя «#{name}»"}
-			let(:heading) { "Страница пользователя «#{name}»"}
-		end
-		it_should_behave_like 'данные пользователя'
-	end
-
-	shared_examples_for 'страница редактирования' do |name|
-		it_should_behave_like 'страница' do
-			let(:title) { "Редактирование пользователя «#{name}»" }
-		end
-		it_should_behave_like 'форма редактирования'
-	end
-
-	shared_examples_for 'данные пользователя' do
-	end
-
-	shared_examples_for 'форма редактирования' do
-		it { should have_selector('label',text:'Имя') }
-		it { should have_selector(:xpath, "//input[@name='user[name]']") }
-
-		it { should have_selector('label', text:'Электронная почта') }
-		it { should have_selector(:xpath, "//input[@name='user[email]']") }
-
-		it { should have_selector('label', text:'Пароль') }
-		it { should have_selector(:xpath, "//input[@name='user[password]']") }
-
-		it { should have_selector('label', text:'Подтверждение пароля') }
-		it { should have_selector(:xpath, "//input[@name='user[password_confirmation]']") }
-
-		it { should have_link('Отмена') }
 	end
 
 
@@ -95,84 +46,18 @@ describe 'Страницы пользователя,' do
 
 		describe 'not_signed_in_users(),' do
 			context 'невошедший пользователь,' do
-				before { visit login_path }
+				before { visit user_path(user) }
 				it_should_behave_like 'страница входа'
 			end
 
 			context 'вошедший пользователь,' do
 				before {
 					sign_in user
-					visit login_path
+					visit user_path(user)
 				}
-				it_should_behave_like 'появление flash-сообщения', 'Вы уже вошли на сайт'
-				it_should_behave_like 'страница пользователя' do
-					let(:name) { user.name }
-				end
-			end
-		end
-
-		describe 'signed_in_users(),' do
-			context 'невошедший пользователь,' do
-				before { visit user_path(user) }
-				it_should_behave_like 'требование входа'
-			end
-
-			context 'вошедший пользователь,' do
-				before { 
-					sign_in user
-					visit user_path(user) 
-				}
-				it_should_behave_like 'страница пользователя' do
-					let(:name) { user.name }
-				end
-			end
-		end
-
-		describe 'editor_users(),' do
-			context 'редактор,' do
-				before { 
-					sign_in user
-					visit edit_user_path(user)
-				}
-				it_should_behave_like 'страница редактирования' do
-					let(:name) { user.name }
-				end
-			end
-
-			context 'не редактор,' do
-				before { visit edit_user_path(other_user) }
-				it_should_behave_like 'появление flash-сообщения', 'Нельзя редактировать другого пользователя'
-				it_should_behave_like 'страница пользователя' do
-					let(:name) { other_user.name }
-				end
-			end
-		end
-
-		describe 'admin_users(),' do
-			context 'админ,' do
-				before { sign_in admin }
-				specify{ expect(delete user_path(user)).to change(User,:count).by(-1) }
-			end
-
-			context 'не админ,' do
-				before { sign_in user }
-				specify{ expect(delete user_path(other_user)).not_to change(User,:count).by(-1) }
-			end			
-		end
-
-		describe 'reject_nil_target(),' do
-			before { sign_in user  }
-
-			context 'объекта не существует' do
-				before { visit user_path(wrong_id) }
-				it_should_behave_like 'появление flash-сообщения', 'Несуществующий объект'
-				it_should_behave_like 'главная страница'
-			end
-
-			context 'объект существует' do
-				before { visit user_path(user) }
-				it_should_behave_like 'страница пользователя' do
-					let(:name) { user.name }
+				it_should_behave_like 'страница c названием' do
+					#let(:the_title) { "Страница пользователя «#{user.name}»"}
+					let(:the_user) { user }
 				end
 			end
 		end
