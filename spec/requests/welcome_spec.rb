990ba26d79638545e72f77b3f7b8de1101956883
Андрей Кумыков <aakumykov@yandex.ru>
2015-12-23 10:39:46 +0700

    Тест статических страниц с учётом пользователей.

--- spec/requests/welcome_spec.rb
+++ spec/requests/welcome_spec.rb
@@ -4,13 +4,21 @@ describe 'Статические welcome-страницы,' do
 	subject { page }
 	
 	shared_examples_for 'all_welcome_pages' do
-		it { should have_link('Главная страница', href: home_path) }
-		it { should have_link('Карточки', href: cards_path) }
-		it { should have_link('Пользователи', href: users_path) }
-		it { should have_link('О проекте', href: about_path) }
-		it { should have_link('Помощь', href: help_path) }
-		it { should have_link('Вход', href: login_path) }
-		it { should have_link('Регистрация', href: new_user_path) }
+		context 'гость,' do
+			it { should have_link('Главная страница', href: home_path) }
+			it { should have_link('Карточки', href: cards_path) }
+			it { should have_link('О проекте', href: about_path) }
+			it { should have_link('Помощь', href: help_path) }
+			it { should have_link('Вход', href: login_path) }
+			it { should have_link('Регистрация', href: new_user_path) }
+		end
+		
+		context 'пользователь,' do
+			let(:user) { FactoryGirl.create(:user) }
+			before { sign_in user }
+		
+			it { should have_link('Пользователи', href: users_path) }
+		end
 	end
 
 	describe 'Главная страница,' do
