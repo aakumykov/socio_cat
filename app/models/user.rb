245523c89e62ceb21ca661053f05cb2fa1c9297d
Андрей Кумыков <aakumykov@yandex.ru>
2015-12-27 17:39:47 +0700

    Починил аутентификацию.
     Причина в в создании токена памяти при сохранении (т.е. и обновлении)
    вместо "при создании".

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 
 	has_many :cards, inverse_of: :user
 
-	before_save :create_remember_token
+	before_create :create_remember_token
 	before_save { email.downcase! }
 
 	validates :name, { 
