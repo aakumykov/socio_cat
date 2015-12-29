2c57cecce1485dd0b1f3d559d31b5e4036dba8e9
Андрей Кумыков <aakumykov@yandex.ru>
2015-12-29 22:20:55 +0700

    Сгенерировал таблицу Категории.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151224044420) do
+ActiveRecord::Schema.define(version: 20151229151730) do
 
   create_table "cards", force: true do |t|
     t.string   "title"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20151224044420) do
   end
 
   add_index "cards", ["user_id"], name: "index_cards_on_user_id"
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.text     "name"
