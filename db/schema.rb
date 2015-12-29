cecbdcbf42ed95d7dc9e3ba76614233354af2d53
Андрей Кумыков <aakumykov@yandex.ru>
2015-12-29 23:18:17 +0700

    Сгенерировал модель Категория, индекс имён к ней.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151229151730) do
+ActiveRecord::Schema.define(version: 20151229161643) do
 
   create_table "cards", force: true do |t|
     t.string   "title"
@@ -26,9 +26,12 @@ ActiveRecord::Schema.define(version: 20151229151730) do
   create_table "categories", force: true do |t|
     t.string   "name"
     t.text     "description"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "categories", ["name"], name: "index_categories_on_name", unique: true
 
   create_table "users", force: true do |t|
     t.text     "name"
