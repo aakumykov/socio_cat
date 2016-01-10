431e1ca1751218ba620c582255b4c43cd435d10a
Андрей Кумыков <aakumykov@yandex.ru>
2016-01-10 17:00:35 +0700

    Добавляет имя на кнопку профиля

--- spec/requests/welcome_spec.rb
+++ spec/requests/welcome_spec.rb
@@ -2,7 +2,27 @@ require 'spec_helper'
 
 describe 'Статические welcome-страницы,' do
 	subject { page }
-	
+
+	shared_examples_for 'все статические страницы' do
+		context 'гость,' do
+			it { should have_link('Главная страница', href: home_path) }
+			it { should have_link('Категории', href: categories_path) }
+			it { should have_link('Карточки', href: cards_path) }
+			it { should have_link('О проекте', href: about_path) }
+			it { should have_link('Помощь', href: help_path) }
+			it { should have_link('Вход', href: login_path) }
+			it { should have_link('Регистрация', href: new_user_path) }
+		end
+		
+		context 'пользователь,' do
+			let(:user) { FactoryGirl.create(:user) }
+			before { sign_in user }
+		
+			it { should have_link('Пользователи', href: users_path) }
+			#it { should have_link('Мой профиль') }
+			it { should have_link("Профиль [#{user.name}]", href: user_path(user)) }
+		end
+	end	
 
 	describe 'Главная страница,' do
 		before { visit home_path }
