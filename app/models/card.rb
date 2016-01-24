be148672e659ffb0d9e7ba9cbf6ead1eedd86d80
Андрей Кумыков <aakumykov@yandex.ru>
2016-01-25 00:47:08 +0700

    Card#fill_categories(array)

--- app/models/card.rb
+++ app/models/card.rb
@@ -7,7 +7,6 @@ class Card < ActiveRecord::Base
 
 
 	before_validation { |m| m.remove_trailing_spaces(:title,:content) }
-	after_save :categorize
 
 
 	validates :title, {
@@ -25,28 +24,11 @@ class Card < ActiveRecord::Base
 		format: /\A[0-9]+\z/,
 	}
 
-	def categorize(cat_id_list=nil)
-		#puts "=========> cat_id_list: |#{cat_id_list}|"
-
-		if cat_id_list.is_a?(Array) && !cat_id_list.compact.empty?
-			# удаляю то, что не даёт цифровой id
-			cat_id_list.reject! {|i| !i.respond_to?(:id) && !i.respond_to?(:to_i)}
-			#puts "==========> cat_id_list.reject!: #{cat_id_list}"
-			
-			cat_id_list.map! {|i| i.respond_to?(:id) && i.id or i}
-			cat_id_list.map! {|i| i.respond_to?(:to_i) && i.to_i or i}
-			#puts "==========> cat_id_list.map!: #{cat_id_list}"
-
-			return false if cat_id_list.empty?
-
-			current_cat_ids = Category.where(id:cat_id_list).pluck(:id)
-			#puts "==========> current_cat_ids: #{current_cat_ids}"
-
-			all_cat_ids = cat_id_list.concat(current_cat_ids).map!{|i| i.to_i}.uniq!
-			#puts "==========> all_cat_ids: #{all_cat_ids}"
-
-			all_cats = Category.find(all_cat_ids)
-			self.categories = all_cats
+	# к этому публичному методу не ведёт (и не должен вести) никакой маршрут
+	def fill_categories(list)
+		self.categories = []
+		list.each do |id|
+			self.cc_relations.create(category_id: id)
 		end
 	end
 end
