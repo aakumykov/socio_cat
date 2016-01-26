0ce30479051e35650e4d7619a6953203c81e728e
Андрей Кумыков <aakumykov@yandex.ru>
2016-01-26 10:23:59 +0700

    Снарядил Почтальона

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,14 @@
 class UserMailer < ApplicationMailer
+	default from: 'user@example.org'
+
+	def welcome_email(user)
+		@user = user
+		@url = 'http://localhost:3000/login'
+		#@url = login_url
+		#@url = login_path
+		mail(
+			to: @user.email,
+			subject: 'Добро пожаловать в соционический каталог'
+		)
+	end
 end
