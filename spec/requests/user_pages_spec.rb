b0664049cfcfaf85b9f4f7ed969f091087e20572
Андрей Кумыков <aakumykov@yandex.ru>
2016-01-31 19:35:48 +0700

    Обдумал защиту users#new_password

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -499,58 +499,47 @@ describe 'Страницы пользователя,' do
 						}
 			 		end
 
-			# 		pending 'продумать зыщиту методов'
-			# 		pending 'валидация длины пароля' # работают, но проверять!
-			# 		pending 'post to new_password'
+					pending 'продумать защиту методов'
+
+					# pending 'post to new_password' должно работать только с флагом.
+					# подумал: сейчас users#new_pasword "защищён" только html-страницей,
+					# а нужно сделать флаго0зависимое поведение
 				end
 
-			# 	describe 'с верными параметрами,' do
-			# 		before {
-			# 			reset_params = user.reset_password
-			# 			visit url_for_password_reset(reset_code:reset_params[:reset_code])
-			# 		}
-			# 		it_should_behave_like 'страница_с_названием' do
-			# 			let(:title) { 'Создание нового пароля' }
-			# 			let(:heading) { title }
-			# 		end
-			# 		it { should have_field 'Новый пароль' }
-			# 		it { should have_field 'Подтверждение нового пароля' }
-			# 		it { should have_xpath "//input[@type='submit' and @value='#{new_password_button}']" }
-			# 		it { should have_xpath "//input[@type='hidden' and @id='user_reset_code']" }
-			# 		it { should have_xpath "//input[@type='hidden' and @id='user_id']" }
+				describe 'с верными параметрами,' do
+					before {
+						reset_params = user.reset_password
+						visit url_for_password_reset(reset_code:reset_params[:reset_code])
+					}
+					it_should_behave_like 'страница_с_названием' do
+						let(:title) { 'Создание нового пароля' }
+						let(:heading) { title }
+					end
+					it { should have_field 'Новый пароль' }
+					it { should have_field 'Подтверждение нового пароля' }
+					it { should have_xpath "//input[@type='submit' and @value='#{new_password_button}']" }
+					it { should have_xpath "//input[@type='hidden' and @id='user_reset_code']" }
+					it { should have_xpath "//input[@type='hidden' and @id='user_id']" }
 
-			# 		describe 'установка нового пароля,' do
-			# 			describe 'корректного,' do
-			# 				before {
-			# 					fill_in 'Новый пароль', with: new_password
-			# 					fill_in 'Подтверждение нового пароля', with: new_password
-			# 					click_button "#{new_password_button}"
-			# 				}
-			# 				specify{
-			# 					expect(user.reload.authenticate(new_password)).not_to be_false
-			# 				}
-			# 				it_should_behave_like 'flash-сообщение', 'success', 'Новый пароль установлен'
-			# 				it_should_behave_like 'страница_входа'
-			# 			end
+					describe 'установка нового пароля,' do
+						describe 'корректного,' do
+							before {
+								fill_in 'Новый пароль', with: new_password
+								fill_in 'Подтверждение нового пароля', with: new_password
+								click_button "#{new_password_button}"
+							}
+							specify{
+								expect(user.reload.authenticate(new_password)).not_to be_false
+							}
+							it_should_behave_like 'flash-сообщение', 'success', 'Новый пароль установлен'
+							it_should_behave_like 'страница_входа'
+						end
 
-			# 			pending 'некорректного'
-			# 		end
-			# 	end
+						pending 'некорректного'
+						# pending 'валидация длины пароля' # работают, но проверять!
+					end
+				end
 			end
 		end
 	end
-
-	###expect(page).to have_current_path(post_comments_path(post))
-
-	# describe 'проверка1' do
-	# 	let(:reset_url) { url_for_password_reset(reset_code: User.new_remember_token) }
-	# 	before {
-	# 		www_user
-	# 		visit reset_url
-	# 	}
-	# 	it_should_behave_like 'flash-сообщение', 'error', 'Вы - зарегистрированный пользователь'
-	# 	it_should_behave_like 'страница_пользователя', 'владелец' do
-	# 		let(:the_user) { user }
-	# 	end
-	# end
 end
