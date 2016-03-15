d1375959ebf9a0129fa87c87162d4361f179e179
Андрей Кумыков <aakumykov@yandex.ru>
2016-03-15 14:52:42 +0700

    Инфраструктура Объектов

--- app/controllers/matters_controller.rb
+++ app/controllers/matters_controller.rb
@@ -1,22 +1,62 @@
 class MattersController < ApplicationController
-  def index
-  end
 
-  def new
-  end
+	before_action :reject_nil_target, only: [:show, :edit, :update, :destroy]
+	#before_action :signed_in_users, only: []
+	#before_action :editor_users, only: []
+	before_action :admin_users, only: [:new, :create, :edit, :update, :destroy, :block]
 
-  def create
-  end
 
-  def show
-  end
+	def index
+		@list = Matter.all
+	end
 
-  def edit
-  end
+	def new
+		@obj = Matter.new
+	end
 
-  def update
-  end
+	def create
+		@obj = Matter.new(matter_params)
+		if @obj.save
+			flash[:success] = "Создан объект «#{@obj.name}»"
+			redirect_to matter_path @obj
+		else
+			render :edit
+		end
+	end
 
-  def destroy
-  end
+	def show
+		@obj = Matter.find_by(id: params[:id])
+	end
+
+	def edit
+		@obj = Matter.find_by(id: params[:id])
+	end
+
+	def update
+		@obj = Matter.find_by(id: params[:id])
+
+		if @obj.update_attributes(matter_params)
+			flash[:success] = "Объект «#{@obj.name}» изменён"
+			redirect_to @obj
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@obj = Matter.find_by(id: params[:id])
+
+		if @obj.destroy
+			flash[:notice] = "Удалён объект «#{@obj.name}»"
+			redirect_to matters_path
+		else
+			flash[:danger] = "Ошибка удаления «#{@obj.name}»"
+			redirect_to @obj
+		end
+	end
+
+	private
+		def matter_params
+			params.require(:matter).permit(:name)
+		end
 end
